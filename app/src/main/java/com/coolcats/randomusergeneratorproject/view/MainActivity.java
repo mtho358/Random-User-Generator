7afbb3f6ef34dd6d2a1de0e6a4b60c9255ed5e60
mtho358 <Mar19t98@>
2021-05-05 20:42:57 -0400

    Layouts created

--- app/src/main/java/com/coolcats/randomusergeneratorproject/view/MainActivity.java
+++ app/src/main/java/com/coolcats/randomusergeneratorproject/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.coolcats.randomusergeneratorproject;
+package com.coolcats.randomusergeneratorproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.coolcats.randomusergeneratorproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
